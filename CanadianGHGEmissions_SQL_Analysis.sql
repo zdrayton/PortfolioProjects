0574fbf98ea713713f1ffee59783baaf8d086765
Zachary Drayton <55886195+zdrayton@users.noreply.github.com>
2023-12-27 12:58:11 -0500

    Update CanadianGHGEmissions_SQL_Analysis.sql

--- CanadianGHGEmissions_SQL_Analysis.sql
+++ CanadianGHGEmissions_SQL_Analysis.sql
@@ -5,19 +5,19 @@ SELECT ROUND(SUM(CO2eq),2) AS Total_CO2eq_Mt        -- Select the rounded sum of
 FROM ghg_econ_can_prov_terr                         -- From the table ghg_econ_can_prov_terr
 WHERE                                               -- Filter rows where Region is 'Canada' and Source is 'National Inventory Total'
 	Region= 'Canada'
-    AND Source = 'National Inventory Total';
+	AND Source = 'National Inventory Total';
 
 
 /***********************************************
 * Total CO2eq Emissions per year over 30 years *
 ***********************************************/
 SELECT 
-    Year,                                           -- Select the Year column
-    ROUND(SUM(CO2eq),2) AS Total_CO2eq_Mt           -- Select the rounded sum of CO2eq column and alias it as Total_CO2eq_Mt
+	Year,                                       -- Select the Year column
+    	ROUND(SUM(CO2eq),2) AS Total_CO2eq_Mt       -- Select the rounded sum of CO2eq column and alias it as Total_CO2eq_Mt
 FROM ghg_econ_can_prov_terr                         -- From the table ghg_econ_can_prov_terr
 WHERE                                               -- Filter rows where Region is 'Canada' and Source is 'National Inventory Total' 
 	Region= 'Canada'
-    AND Source = 'National Inventory Total'
+	AND Source = 'National Inventory Total'
 GROUP BY Year;                                      -- Group results by Year
 
 
@@ -25,13 +25,13 @@ GROUP BY Year;                                      -- Group results by Year
 * Total CO2eq Per Province sorted from highest to lowest *
 *********************************************************/
 SELECT 
-    Region, 
-    ROUND(SUM(CO2eq),2) AS Total_CO2eq_Mt           -- Select the rounded sum of CO2eq column and alias it as Total_CO2eq_Mt
+	Region, 
+	ROUND(SUM(CO2eq),2) AS Total_CO2eq_Mt       -- Select the rounded sum of CO2eq column and alias it as Total_CO2eq_Mt
 FROM ghg_econ_can_prov_terr                         -- From the table ghg_econ_can_prov_terr
 WHERE                                               -- Filter rows where Region is not 'Canada', Source is 'National Inventory Total', and Total = True  
 	Region <> 'Canada'
-    AND Source = 'Provincial Inventory Total'
-    AND Total = "True"
+	AND Source = 'Provincial Inventory Total'
+	AND Total = "True"
 GROUP by Region                                     -- Group results by Region
 ORDER BY Total_CO2eq_Mt DESC;                       -- Order results by Total_CO2eq_MT in descending order
 
@@ -40,15 +40,15 @@ ORDER BY Total_CO2eq_Mt DESC;                       -- Order results by Total_CO
 * Total CO2eq by Source  *
 *************************/
 SELECT 
-    Source, 
-    ROUND(SUM(CO2eq),2) AS Total_CO2eq_Mt           -- Select the rounded sum of CO2eq column and alias it as Total_CO2eq_Mt
+	Source,					    -- Extract data from Sourec column 
+    	ROUND(SUM(CO2eq),2) AS Total_CO2eq_Mt       -- Select the rounded sum of CO2eq column and alias it as Total_CO2eq_Mt
 FROM ghg_econ_can_prov_terr                         -- From the table ghg_econ_can_prov_terr
 WHERE                                               -- Filter rows where Region is not 'Canada', Source is NOT the 'National Inventory Total' or Territorial Inventory Total, Category, Sub-category,Sub-sub-category are blank and the Total = True
 	Region <> 'Canada'                               
-    AND Source NOT IN ('Provincial Inventory Total', 'Territorial Inventory Total')
-    AND Category = ""
+    	AND Source NOT IN ('Provincial Inventory Total', 'Territorial Inventory Total')
+    	AND Category = ""
 	AND `Sub-category` = ""
-    AND `Sub-sub-category` = ""
-    AND Total = "True"
+    	AND `Sub-sub-category` = ""
+    	AND Total = "True"
 GROUP by Source                                     -- Group results by Source
 ORDER BY Total_CO2eq_Mt DESC;                       -- Order results by Total_CO2eq_MT in descending order
